fa84ad18e6cd66fdf1ceb163b0fbd3812481ba17
Josh Johnson <josh.johnson@truecommerce.com>
2020-03-26 23:01:21 -0400

    Use Anonymous Functions

--- wp-theme/functions.php
+++ wp-theme/functions.php
@@ -1,24 +1,27 @@
 <?php
 
-function readynext_after_setup_theme() {
+add_action('after_setup_theme', function () {
     add_theme_support('automatic-feed-links');
 
     add_theme_support('custom-header', array(
-        'header-text' => true
+        'header-text' => false
     ));
 
-    add_theme_support('editor-styles');
-    add_editor_style('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
+    add_theme_support('editor-style');
+
+    add_filter('mce_css', function ($mce_css) {
+        if (!empty($mce_css)) {
+            $mce_css .= ',';
+        }
+
+        $mce_css .= 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css';
+    });
 
     add_theme_support('title-tag');
-}
+});
 
-add_action('after_setup_theme', 'readynext_after_setup_theme');
-
-function readynext_init() {
+add_action('init', function () {
     register_nav_menus(array(
         'primary' => __('Primary')
     ));
-}
-
-add_action('init', 'readynext_init');
+});
