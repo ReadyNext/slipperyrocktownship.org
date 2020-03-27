1dd32107ed280e91d20aebb7e58eeb4634abf273
Josh Johnson <josh.johnson@truecommerce.com>
2020-03-26 23:07:09 -0400

    Fix Style Location

--- wp-theme/template-parts/masthead/jumbotron.php
+++ wp-theme/template-parts/masthead/jumbotron.php
@@ -5,13 +5,13 @@
         $header_image_url = get_header_image();
 
         $caption = wp_get_attachment_caption(attachment_url_to_postid($header_image_url));
-        $jumbotron_wrapper_style = 'style="background-image:url(' . $header_image_url .');"';
+        $jumbotron_wrapper_style = 'style="background-image:url(' . $header_image_url .');background-position:center;"';
     }
 ?>
 
 <div class="jumbotron-wrapper" <?= $jumbotron_wrapper_style ?>>
 
-    <div class="jumbotron jumbotron-fluid" style="background:transparent;background-position:center;margin-bottom:0;">
+    <div class="jumbotron jumbotron-fluid" style="background:transparent;margin-bottom:0;">
         <div class="container text-center text-light" style="text-shadow:2px 2px #222222;">
             <h1 class="display-4">
                 <?= get_bloginfo('name') ?>
